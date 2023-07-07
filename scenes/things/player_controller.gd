78734a8adb0e5bf612a4ee701b277020afaede58
Noah Petrides <nopetrides@gmail.com>
2023-07-07 13:59:36 -0700

    Mouse and mouse movement

---
+++ scenes/things/player_controller.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed = 400
+# Called when the node enters the scene tree for the first time.
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	look_at(position+velocity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	pass
