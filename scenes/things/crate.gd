9665f86cd9118b06623eea8f287171a070e0f359
KarlTheCool <k@rlphilli.ps>
2023-07-07 17:34:52 -0700

    crate go boom

--- scenes/things/crate.gd
+++ scenes/things/crate.gd
@@ -13,6 +13,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
+func _no_collide():
+	$CollisionShape2D.disabled = true
+
 func destroy():
-	queue_free()
+	call_deferred("_no_collide")
+	$Animate.current_animation = "boom"
+	$Animate.play()
+	$Animate.animation_finished.connect(func(_name): self.queue_free(), CONNECT_ONE_SHOT)
