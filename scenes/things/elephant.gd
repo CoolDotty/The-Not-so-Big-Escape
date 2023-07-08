820ab409bdd4022dfd9cf468edcc77184ac698d3
liam000001206 <92518491+liam000001206@users.noreply.github.com>
2023-07-07 23:02:26 -0700

    aaa

--- scenes/things/elephant.gd
+++ scenes/things/elephant.gd
@@ -2,12 +2,7 @@ extends CharacterBody2D
 
 const lore_name = "Whiskers"
 
-enum _action_state {
-	none,
-	is_patrolling,
-	is_spooked,
-	is_alerted
-}
+
 
 var is_spooked = false
 
@@ -15,27 +10,54 @@ var is_spooked = false
 const patrol_speed = 150
 const spook_speed = 500
 
+
+
+# Region: Nav
+
+var path: Array = []
+var levelNavigation: NavigationAgent2D = null
+var player = null
+
+@onready var _map : TileMap = $"../puzzle_tiles"
+@onready var _navAgent : NavigationAgent2D = $NavigationAgent2D
 var _explore_location = Vector2(0.0, 0.0)
 
+# End Region
+
+# Region: Action State
+
+enum _action_state {
+	none,
+	is_patrolling,
+	is_spooked,
+	is_alerted
+}
+var _current_action_state = _action_state.none
+
+
+
+# End Region
+
 func _ready():
+	_navAgent.set_navigation_map(_map)
 	Global.mouse_squeaked.connect(
 		func(mouse):
-			_explore_location = mouse.get_position()
-			_explore_sound_source(_explore_location)
+			_explore_location = mouse.global_transform.origin
+			_navAgent.target_position = _explore_location
+			Set_action_state(_action_state.is_alerted)
+			_on_alerted()
 	)
 
 func _physics_process(delta):
-	if is_spooked:
-		velocity = Vector2(spook_speed, 0).rotated(rotation)
-	else:
-		velocity = Vector2(patrol_speed, 0).rotated(rotation)
+	
+	_action_state_tick(_current_action_state)
 	
 	var prev_pos = position
 	
 	var hit = move_and_slide()
 	
-	if (position - prev_pos).length() < 1:
-		rotation_degrees += 90
+	#if (position - prev_pos).length() < 1:
+	#	rotation_degrees += 90
 
 
 
@@ -53,14 +75,23 @@ func _on_vision_body_entered(body):
 
 func _on_spooked(min = 1.0, max = 2.0):
 	is_spooked = true
+	Set_action_state(_action_state.is_spooked)
 	var SpookTimer = $vision/SpookTimer
 	var rng = RandomNumberGenerator.new()
 	var spook_random_number = rng.randf_range(min, max)
 	SpookTimer.set_wait_time(spook_random_number)
 	SpookTimer.start()
+	
+func _on_alerted():
+	var SpookTimer = $vision/SpookTimer
+	var rng = RandomNumberGenerator.new()
+	var spook_random_number = rng.randf_range(2.0, 3.0)
+	SpookTimer.set_wait_time(spook_random_number)
+	SpookTimer.start()
 
 func _on_spook_timer_timeout():
 	is_spooked = false
+	Set_action_state(_action_state.is_patrolling)
 
 func _explore_sound_source(location:Vector2):
 	var temp_direction: Vector2
@@ -68,30 +99,38 @@ func _explore_sound_source(location:Vector2):
 	temp_direction = temp_direction.normalized()
 	velocity = temp_direction * spook_speed
 	
-func check_action_state(state: _action_state):
+func _action_state_tick(state: _action_state):
 	match state:
 		_action_state.none:
-			pass
+			_patrolling_tick()
+			
 		_action_state.is_patrolling:
-			pass
+			_patrolling_tick()
+			
 		_action_state.is_spooked:
-			pass
+			_spooked_tick()
+			
 		_action_state.is_alerted:
-			pass
+			_alerted_tick()
 		_:
 			pass
 	return
 	
+func Set_action_state(state: _action_state):
+	_current_action_state = state
+
 func _patrolling_tick():
 	velocity = Vector2(patrol_speed, 0).rotated(rotation)
-	pass
 	
 func _spooked_tick():
 	velocity = Vector2(spook_speed, 0).rotated(rotation)
-	pass
 	
 func _alerted_tick():
-	#_explore_sound_source()
-	pass
+	var dir = _navAgent.get_next_path_position() - position
+	velocity = dir.normalized() * patrol_speed
+	if(_navAgent.is_navigation_finished()):
+		Set_action_state(_action_state.is_patrolling)
+	move_and_slide()
+	
 	
 
