0b450cb594671c10b521973f729e444daa81c9cd
KarlTheCool <k@rlphilli.ps>
2023-07-08 01:07:06 -0700

    cooooool transitions

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,11 @@
 extends Node
 
+@onready var current_scene = get_child(0)
+
+
+var trans_in = preload("res://scenes/DiamondTransitionIn.tres")
+var trans_out = preload("res://scenes/DiamondTransitionOut.tres")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +18,26 @@ func _process(delta):
 
 
 func _change_scenes(next_scene: PackedScene):
-	get_child(0).queue_free()
-	add_child(next_scene.instantiate())
+	$TransitionLayer.visible = true
+	trans_in.set_shader_parameter("progress", 0.0)
+	$TransitionLayer/ColorRect.material = trans_in
+	for i in range(0, 60):
+		$TransitionLayer/ColorRect.material.set_shader_parameter(
+			"progress", 
+			$TransitionLayer/ColorRect.material.get_shader_parameter("progress") + (1.0 / 60)
+		)
+		await get_tree().physics_frame
+	
+	current_scene.queue_free()
+	current_scene = next_scene.instantiate()
+	add_child(current_scene)
+	
+	trans_out.set_shader_parameter("progress", 0.0)
+	$TransitionLayer/ColorRect.material = trans_out
+	for i in range(0, 60):
+		$TransitionLayer/ColorRect.material.set_shader_parameter(
+			"progress", 
+			$TransitionLayer/ColorRect.material.get_shader_parameter("progress") + (1.0 / 60)
+		)
+		await get_tree().physics_frame
+	$TransitionLayer.visible = false
--- scenes/storymode/slide.gd
+++ scenes/storymode/slide.gd
@@ -3,3 +3,5 @@ class_name Slide
 
 @export var pic: Texture
 @export_multiline var text: String
+enum FX { None, Rock, Shake, Zoom}
+@export var effect: FX = FX.None
