167a539c094aef4aa9ff5ab5bd42db2fbcf306e1
liam000001206 <92518491+liam000001206@users.noreply.github.com>
2023-07-09 00:26:09 -0700

    reupload triggers

--- scenes/things/pressurePlate.gd
+++ scenes/things/pressurePlate.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Interactable
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,4 +17,5 @@ func _on_body_entered(body):
 	#print("pressure plate!")
 	#add sound here
 	#add effect here
+	_on_interact(body)
 	pressurePlate_Active.emit(body)
