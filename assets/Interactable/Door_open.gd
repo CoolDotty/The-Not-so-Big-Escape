5bb925a0596ce47777f29963ca6170ffa71a52ba
liam000001206 <92518491+liam000001206@users.noreply.github.com>
2023-07-09 02:55:51 -0700

    update door collision

--- assets/Interactable/Door_open.gd
+++ assets/Interactable/Door_open.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 
 @export var opening : bool
@@ -8,7 +8,9 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if(require_triggers_to_open <= 0): opening = true
+	
+	if(require_triggers_to_open <= 0):
+		Set_isDoorOpen(true)
 	for each_trigger in Triggers:
 		each_trigger.on_interact.connect(Interacted)
 	
@@ -32,6 +34,12 @@ func Interacted(Instigator, trigger : Interactable):
 		trigger.on_interact.disconnect(Interacted)
 		require_triggers_to_open = require_triggers_to_open - 1
 		if(require_triggers_to_open <= 0):
-			opening = true
-			$CollisionShape2D.disabled
+			Set_isDoorOpen(true)
 	pass # Replace with function body.
+
+func Set_isDoorOpen(enable : bool):
+	opening = enable
+	call_deferred("_setCollisionDisable", enable)
+	
+func _setCollisionDisable(enable : bool):
+	$StaticBody2D/CollisionShape2D.disabled = enable
