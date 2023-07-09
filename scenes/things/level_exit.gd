a54fe7150ac3c72e41fdabd83976ddfd67aefee6
KarlTheCool <k@rlphilli.ps>
2023-07-08 18:28:50 -0700

    door

--- scenes/things/level_exit.gd
+++ scenes/things/level_exit.gd
@@ -9,12 +9,17 @@ func _ready():
 	pass # Replace with function body.
 
 
+var opening = false
+var f = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if opening:
+		f = min(f + 0.1, 3)
+		$Icon.frame = int(f)
 
 
 func _on_body_entered(body):
+	opening = true
 	if body.get("lore_name") == "Trunks":
 		assert(next_scene, "No next_scene assigned to exit")
 		Global.scene_ended.emit(next_scene)
